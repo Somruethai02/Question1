1eae0f002d869b8ec838eed1785acfeb7713332f
Somruthai <Somruthai_muchan@hotmail.com>
2017-01-05 00:43:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rtc/somruethaianusa/question1/ShowScoreActivity.java
@@ -0,0 +1,69 @@
+package com.example.rtc.somruethaianusa.question1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class ShowScoreActivity extends AppCompatActivity {
+
+    private TextView txtShowScore;
+    private int intMyScore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_score);
+
+        txtShowScore = (TextView) findViewById(R.id.txtShowScore);
+
+        //Get Value From QuestionActivity
+        intMyScore = getIntent().getExtras().getInt("Score");
+
+        txtShowScore.setText(Integer.toString(intMyScore) + "/10" );
+
+
+    }   // onCreate
+
+
+    public void clickPlay(View view) {
+
+        Intent objIntent = new Intent(ShowScoreActivity.this, QuestionActivity.class);
+        startActivity(objIntent);
+
+    }
+
+    public void clickExit(View view) {
+
+        finish();
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_show_score, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+} // Main Class
